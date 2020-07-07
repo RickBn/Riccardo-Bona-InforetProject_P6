17556bd2a1f1f919a837c0c2cb35630beded2b75
Riccardo Bona <66834941+RickBn@users.noreply.github.com>
2020-07-07 18:29:09 +0200

    Add files via upload

--- Project.py
+++ Project.py
@@ -65,28 +65,29 @@ from gensim.models import Word2Vec
 from gensim.models.keyedvectors import KeyedVectors
 from gensim.models.callbacks import CallbackAny2Vec
 import seaborn as sns
+import matplotlib
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 
 seq = []
 for i in range(len(D)):
     seq.append(D['mechanics'][i])
 
-
-class callback(CallbackAny2Vec):
-
-    # Callback to print loss after each epoch
-    def __init__(self):
-        self.epoch = 0
-
-    def on_epoch_end(self, model):
-        loss = model.get_latest_training_loss()
-        if self.epoch == 0:
-            print('Loss after epoch {}: {}'.format(self.epoch, loss))
-        else:
-            print('Loss after epoch {}: {}'.format(self.epoch, loss - self.loss_previous_step))
-        self.epoch += 1
-        self.loss_previous_step = loss
-
+# class callback(CallbackAny2Vec):
+#
+#     # Callback to print loss after each epoch
+#     def __init__(self):
+#         self.epoch = 0
+#
+#     def on_epoch_end(self, model):
+#         loss = model.get_latest_training_loss()
+#         if self.epoch == 0:
+#             print('Loss after epoch {}: {}'.format(self.epoch, loss))
+#         else:
+#             print('Loss after epoch {}: {}'.format(self.epoch, loss - self.loss_previous_step))
+#         self.epoch += 1
+#         self.loss_previous_step = loss
 
 model = {}
 model = Word2Vec(seq, min_count=1, window=2)
@@ -155,16 +156,16 @@ ax = fig.add_subplot(1, 1, 1)
 
 ax.scatter(df['x'], df['y'], c=clusters, s=30)
 
-# for word, pos in df.iterrows():
-#         ax.annotate(word, pos)
+for word, pos in df.iterrows():
+        ax.annotate(word, pos)
 
-# for word, pos in df.iterrows():
-#     if word in a and word in g:
-#         ax.annotate(word, pos, color='blue')
-#     elif word in a:
-#         ax.annotate(word, pos, color='red')
-#     elif word in g:
-#         ax.annotate(word, pos, color='green')
+for word, pos in df.iterrows():
+    if word in a and word in g:
+        ax.annotate(word, pos, color='blue')
+    elif word in a:
+        ax.annotate(word, pos, color='red')
+    elif word in g:
+        ax.annotate(word, pos, color='green')
 
 for i, j in centroids:
     ax.scatter(i, j, s=50, c='red', marker='+')
@@ -172,11 +173,17 @@ for i, j in centroids:
 
 #2 CLUSTERS
 cluster_df = pd.DataFrame([], index=vocab, columns=[0, 1])
+cluster_df2 = pd.DataFrame([], index=vocab, columns=[0, 1])
 
 for i, words in enumerate(df.iterrows()):
     cluster = labels[i]
     centroid = centroids[cluster]
     cluster_df.loc[words[0]][cluster] = np.linalg.norm(np.array(words[1]) - centroid)
+
+for i, words in enumerate(df.iterrows()):
+    for cluster in labels:
+        centroid = centroids[cluster]
+        cluster_df2.loc[words[0]][cluster] = np.linalg.norm(np.array(words[1]) - centroid)
 
 if np.isnan(cluster_df.loc['Campaign / Battle Card Driven'][0]) == False:
     ame = 0
@@ -203,78 +210,6 @@ AL.insert(1, 'dist_norm', a_d_norm, True)
 g_dists = GL.loc[:]['dist'].values
 g_d_norm = g_dists / max(g_dists)
 GL.insert(1, 'dist_norm', g_d_norm, True)
-
-#3 CLUSTERS
-# num_clusters = 3
-# kmeans = cluster.KMeans(n_clusters=num_clusters)
-#
-# kmeans.fit(px)
-#
-# labels = kmeans.labels_
-# centroids = kmeans.cluster_centers_
-#
-# clusters = np.array(labels)
-#
-# # wcss = []
-# # for i in range(1, 11):
-# #     kmeans = cluster.KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-# #     kmeans.fit(px)
-# #     wcss.append(kmeans.inertia_)
-# # plt.plot(range(1, 11), wcss)
-# # plt.title('Elbow Method')
-# # plt.xlabel('Number of clusters')
-# # plt.ylabel('WCSS')
-# # plt.show()
-#
-# print(labels)
-#
-# print(centroids)
-#
-# print(kmeans.score(px))
-#
-# silhouette_score = metrics.silhouette_score(px, labels, metric='euclidean')
-#
-# print(silhouette_score)
-#
-# # PLOT
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(1, 1, 1)
-#
-# ax.scatter(df['x'], df['y'], c=clusters, s=30)
-#
-# # for word, pos in df.iterrows():
-# #         ax.annotate(word, pos)
-#
-# for word, pos in df.iterrows():
-#     if word in a and word in g:
-#         ax.annotate(word, pos, color='blue')
-#     elif word in a:
-#         ax.annotate(word, pos, color='red')
-#     elif word in g:
-#         ax.annotate(word, pos, color='green')
-#
-# for i, j in centroids:
-#     ax.scatter(i, j, s=50, c='red', marker='+')
-#
-# cluster_df = pd.DataFrame([], index=vocab, columns=[0, 1, 2])
-#
-# for i, words in enumerate(df.iterrows()):
-#     cluster = labels[i]
-#     centroid = centroids[cluster]
-#     cluster_df.loc[words[0]][cluster] = np.linalg.norm(np.array(words[1]) - centroid)
-#
-# AL = cluster_df[cluster_df[2].isna() == False]
-# AL = AL.drop([0,1], axis=1)
-# AL.columns = ['americanlike']
-# AL = AL.sort_values('americanlike')
-#
-# GL = cluster_df[cluster_df[0].isna() == False]
-# GL = GL.drop([1,2], axis=1)
-# GL.columns = ['germanlike']
-# GL = GL.sort_values('germanlike')
-
-
 
 #NN
 
@@ -369,56 +304,45 @@ ax.scatter(df['x'], df['y'], c=colors, s=30)
 ax.scatter(game_centers['american_center'][0], game_centers['american_center'][1], s=50, c='red', marker='P')
 ax.scatter(game_centers['german_center'][0], game_centers['german_center'][1], s=50, c='red', marker='P')
 
+o = pd.DataFrame([], index=df.index, columns=['AC', 'AL', 'GC', 'GL'])
 
-fig, axs = plt.subplots(3)
-fig.suptitle('AMERICAN-LIKE MECHANICS')
+for row in o.iterrows():
+    o.loc[row[0]] = [m_dist.loc[row[0]]['american_center'], cluster_df2.loc[row[0]][ame], m_dist.loc[row[0]]['german_center'], cluster_df2.loc[row[0]][ger]]
 
-axs[0].set_title('Distance from games center')
-axs[0].barh(AC.index[:10], 1 - AC['dist_norm'][:10])
-axs[0].invert_yaxis()
+o_max = max(o.max())
 
-axs[1].set_title('Distance from cluster centroid')
-axs[1].barh(AL.index[:10], 1 - AL['dist_norm'][:10], color='orange')
-axs[1].invert_yaxis()
+o_norm = o / o_max
 
-axs[2].set_title('Distance from nearest game')
-axs[2].barh(AV.index[:10], 1 - AV['dist_norm'][:10], color='green')
-axs[2].invert_yaxis()
+import itertools
+import matplotlib.pyplot as plt
+from matplotlib.widgets import TextBox
 
+all_mechs = list(itertools.chain(*seq))
+m_freq = {m : all_mechs.count(m) for m in o_norm.index}
 
-fig, axs = plt.subplots(3)
-fig.suptitle('GERMAN-LIKE MECHANICS')
+mfs = {k: v for k, v in sorted(m_freq.items(), key=lambda item: item[1], reverse=True)}
 
-axs[0].set_title('Distance from games center')
-axs[0].barh(GC.index[:10], 1 - GC['dist_norm'][:10])
-axs[0].invert_yaxis()
+y = [mech for mech in list(mfs.keys())]
+y.reverse()
 
-axs[1].set_title('Distance from cluster centroid')
-axs[1].barh(GL.index[:10], 1 - GL['dist_norm'][:10], color='orange')
-axs[1].invert_yaxis()
+fig = plt.figure()
+fig.suptitle('AMERICAN-LIKE    GERMAN-LIKE', x=0.54, y=0.94)
+ax = fig.add_subplot(1, 1, 1)
+plt.subplots_adjust(left=0.20, top=0.9)
+plt.axvline(x=0, color='black')
 
-axs[2].set_title('Distance from nearest game')
-axs[2].barh(GV.index[:10], 1 - GV['dist_norm'][:10], color='green')
-axs[2].invert_yaxis()
+ax.barh(y[-20:], np.array(1 - o_norm.loc[y[-20:]]['GL']))
+ax.barh(y[-20:], - np.array(1 - o_norm.loc[y[-20:]]['AL']), color='orange')
 
-#Wargames
+initial_text = '20'
+axbox = plt.axes([0.2, 0.01, 0.06, 0.03])
+text_box = TextBox(axbox, 'Top mechanics', initial=initial_text)
 
-wargames = [12333, 9203, 103885, 227460, 132018, 128996]
-W = D[D['id'].isin(wargames)]
-W = W.reset_index().drop(['index'], 1)
-wargames_title = list(W['title'])
+def submit(text):
+    t = int(text)
+    ax.clear()
+    ax.axvline(x=0, color='black')
+    ax.barh(y[-t:], np.array(1 - o_norm.loc[y[-t:]]['GL']))
+    ax.barh(y[-t:], - np.array(1 - o_norm.loc[y[-t:]]['AL']), color='orange')
 
-wm = {}
-for i in range(len(wargames_title)):
-    wm[wargames_title[i]] = W['mechanics'][i]
-
-w_mec = list(wm.values())
-
-v_w = pd.DataFrame([], index=wargames_title, columns=['x', 'y'])
-
-for game in wargames_title:
-    v_w.loc[game] = df.loc[wm[game]]['x'].mean(), df.loc[wm[game]]['y'].mean()
-
-for game in wargames_title:
-    ax.scatter(v_w.loc[game]['x'], v_w.loc[game]['y'], marker='D')
-    ax.annotate(game, (v_w.loc[game]['x'], v_w.loc[game]['y']))
+text_box.on_submit(submit)
